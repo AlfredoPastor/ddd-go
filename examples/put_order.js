bb44b07e7735baba30d5ac8ce4b7a84ecc5c907f
Alfredo Pastor <cto@losmoviles.com.co>
2022-01-11 19:34:47 -0500

    Examples

---
+++ examples/put_order.js
@@ -0,0 +1,13 @@
+{
+	"id":"61ce390254aa0e0001b3027a",
+    "client_id":"61ce390254aa0e0001b3027b",
+    "address_shipping":"asas",
+    "order_lines": [
+        {
+            "id":"61ce390254aa0e0001b3027c",
+            "product_id": "61ce390254aa0e0001b3027d",
+            "price": 35.0,
+            "quantity": 2
+        }
+    ]
+}
